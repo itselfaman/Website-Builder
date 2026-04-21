83bb66d5999661f9c9d979d1da75a1deec537b21
itsel <itselfaman@gmail.com>
2026-04-21 15:47:55 +0530

    added missing routes

--- server/routes/website.routes.js
+++ server/routes/website.routes.js
@@ -1,10 +1,20 @@
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { generateWebsite } from "../controllers/website.controllers.js";
+import {
+  generateWebsite,
+  getWebsiteById,
+  getAll
+} from "../controllers/website.controllers.js";
 
 const websiteRouter = express.Router();
 
-// ✅ ONLY THIS ROUTE
-websiteRouter.post("/generate", isAuth, generateWebsite);
+// ✅ Generate
+websiteRouter.post("/generate", generateWebsite);
+
+// ✅ Get by ID
+websiteRouter.get("/get-by-id/:id", getWebsiteById);
+
+// ✅ Get all
+websiteRouter.get("/get-all", getAll);
 
 export default websiteRouter;
