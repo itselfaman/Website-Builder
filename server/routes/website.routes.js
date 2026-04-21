d9501f6f386dade859a12257a4844f5227eb43e2
itsel <itselfaman@gmail.com>
2026-04-21 15:00:08 +0530

    cleaned routes fully

--- server/routes/website.routes.js
+++ server/routes/website.routes.js
@@ -1,16 +1,10 @@
-import express from "express"
+import express from "express";
+import isAuth from "../middlewares/isAuth.js";
+import { generateWebsite } from "../controllers/website.controllers.js";
 
-import isAuth from "../middlewares/isAuth.js"
-import { changes, deploy, generateWebsite, getAll, getBySlug, getWebsiteById } from "../controllers/website.controllers.js"
+const websiteRouter = express.Router();
 
+// ✅ ONLY THIS ROUTE
+websiteRouter.post("/generate", isAuth, generateWebsite);
 
-const websiteRouter=express.Router()
-
-websiteRouter.post("/generate",isAuth,generateWebsite)
-websiteRouter.post("/update/:id",isAuth,changes)
-websiteRouter.get("/get-by-id/:id",isAuth,getWebsiteById)
-websiteRouter.get("/get-all",isAuth,getAll)
-websiteRouter.get("/deploy/:id",isAuth,deploy)
-websiteRouter.get("/get-by-slug/:slug",getBySlug)
-
-export default websiteRouter
+export default websiteRouter;
